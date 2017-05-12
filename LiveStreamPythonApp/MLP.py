ebca446ef2441b7d58439d1805e79ae221d0117a
xavier136 <xavier136@hotmail.fr>
2017-05-12 14:22:49 +0100

    Start of the new MLP Class

---
+++ LiveStreamPythonApp/MLP.py
@@ -0,0 +1,9 @@
+from keras.models import Sequential
+
+class MLP(object):
+    """Builds the Multilayers Perceptron"""
+    
+    def __init__(self, layers, activation, ):
+        return super().__init__(*args, **kwargs)
+
+
